57284e295bddca9e48d02d78f73090bc1213b7ba
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-08-07 23:12:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+// variables 
+// CPU + RAM random access memory (16GB RAM laptop
+// 8 GB RAM for my phone)
+
+/*
+Once the program execution is done by the CPU , what ever the data that cpu utilised in RAM will be erased 
+
+RAM is stack of bytes
+1 byte is 8 bits memory
+1 bit 
+0 bit
+
+01000100 => 8bits => 1 byte
+1byte = 8 bits
+1KB = 1024 bytes
+1MB = 1024 KB
+1GB = 1024 MB
+
+
+Memory is a Rack of bytes (boxes of bytes)
+every byte will have a unique address
+*/
+
+
+// 1 + 1 => 2
+// -
+// -
+// -
+// 2 * 7 = 14
+
+// identifiers are english names used in programming languages to hold data in it.
+
+// let, const , var
+
+// let a = 20; // 4 bytes
+// console.log(a); // 20
+// let b = 10; // 4 bytes of memory in the RAM ( 10 )
+// console.log(b); // 10
+
+
+// console.log(b);
+// let b = 20; 
+
+// datatypes 
+
+// number  20, 20.0, 
+// 0 - 1
+// 10, -2, 19.3, .... infinite possible values
+// let a = 20.4;
+// console.log(typeof a)
+// let b = false; // boolean data type
+// // 1 | 0
+// console.log(typeof b)
+
+// all the data which is kept inside ' , " is a string type ``````````` ''''''' """"
+// let x = 'wf0fiprwmffrgmg' ; // char 
+// console.log(typeof x)
+
+// for boolean true | false 
+// for undefined => undefined
+// let x;
+// console.log(typeof x , x); // 90, 12, true, "abc"
+
+// number, boolean, string, undefined
+
+/*
+Identifier declaration rules | variable naming convention
+
+1. a variable should start with _ or alphabet or $
+2. a variable can container numbers, _, $, and alphabets
+*/
+
+
+// operators
+
+/**
+ * 1. unary operator
+ * 2. binary operators
+ * 3. ternary operators
+ * 
+ */
+
+// arithmatic operators +, -, *, / , %
+
+
+// assignemnt operator
+
+let x = 20;
+x += 10; // x = x + 10
